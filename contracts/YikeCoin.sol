117e0f7bdd2e9afe506c0059a98d303eee35d4a1
Ethan <ethan.blumenthal@gmail.com>
2021-07-04 11:47:36 -0500

    Created solve function based on keccak hash.

--- contracts/YikeCoin.sol
+++ contracts/YikeCoin.sol
@@ -4,7 +4,24 @@ pragma solidity 0.8.6;
 import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract YikeCoin is ERC20 {
-    constructor() ERC20("YikeCoin", "YIKE") {
-        _mint(msg.sender, 1000);
+    bytes32 public answer;
+    uint256 public prize;
+
+    constructor(string memory _phrase1, string memory _phrase2) ERC20("YikeCoin", "YIKE") {
+        _mint(address(this), 1000);
+        prize = totalSupply() / 1000;
+        answer = _hash(_phrase1, _phrase2);
+    }
+
+    function solve(string memory _phrase1, string memory _phrase2) public {
+        // Check if hashed passphrase is correct
+        require(_hash(_phrase1, _phrase2) == answer, "Invalid passphrase!");
+        
+        // Transfer 1/1000 of token supply to caller
+        _transfer(address(this), msg.sender, prize);
+    }
+    
+    function _hash(string memory _phrase1, string memory _phrase2) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked(_phrase1, _phrase2));
     }
 }
