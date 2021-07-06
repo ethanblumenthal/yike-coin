10a0d13b1001297642c8e0d12e2524cbfa76069f
Ethan <ethan.blumenthal@gmail.com>
2021-07-05 21:41:11 -0500

    Setup truffle config to deploy to Ropsten.

--- migrations/2_yike_migration.js
+++ migrations/2_yike_migration.js
@@ -1,5 +1,6 @@
 const YikeCoin = artifacts.require('YikeCoin');
+require('dotenv').config();
 
 module.exports = function (deployer) {
-  deployer.deploy(YikeCoin);
+  deployer.deploy(YikeCoin, process.env.PASSPHRASE_1, process.env.PASSPHRASE_2);
 };
